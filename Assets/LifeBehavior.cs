badb0d40d9b2a3d89241d9a34d0d5d471ff9c1a4
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-04-18 00:52:29 +0200

    :sparkles: ajout de la barre de vie functionnel

--- Assets/LifeBehavior.cs
+++ Assets/LifeBehavior.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class LifeBehavior : MonoBehaviour
 {
+	public Sprite ThreeLife;
+	public Sprite TwoLife;
+	public Sprite OneLife;
+	public Sprite ZeroLife;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,17 @@ public class LifeBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(PlayerBehavior.Life == 3){
+        	this.gameObject.GetComponent<SpriteRenderer>().sprite = ThreeLife;
+        }
+        if(PlayerBehavior.Life == 2){
+        	this.gameObject.GetComponent<SpriteRenderer>().sprite = TwoLife;
+        }
+        if(PlayerBehavior.Life == 1){
+        	this.gameObject.GetComponent<SpriteRenderer>().sprite = OneLife;
+        }
+        if(PlayerBehavior.Life == 0){
+        	this.gameObject.GetComponent<SpriteRenderer>().sprite = ZeroLife;
+        }
     }
 }
