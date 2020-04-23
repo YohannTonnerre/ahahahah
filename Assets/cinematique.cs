5f5a3ee1e7e82ea537da9bbcfef1175f905cc86f
Maelle05 <maelle.rabouan@gmail.com>
2020-04-23 14:10:05 +0200

    cinematique :dizzy:

---
+++ Assets/cinematique.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cinematique : MonoBehaviour
+{
+    public void Suivant1 ()
+    {
+        SceneManager.LoadScene("0_2_Cinematique");
+    }
+
+    public void Suivant2 ()
+    {
+        SceneManager.LoadScene("0_3_Cinematique");
+    }
+
+    public void Suivant3 ()
+    {
+        SceneManager.LoadScene("0_4_Cinematique");
+    }
+
+    public void Suivant4 ()
+    {
+        SceneManager.LoadScene("0_5_Cinematique");
+    }
+
+    public void Play ()
+    {
+        SceneManager.LoadScene("1_PacMan");
+    }
+}
