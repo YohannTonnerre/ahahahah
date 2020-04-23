0f2f6638fe4416cf3fa5532054f3e8ae72c73eaf
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-04-23 21:53:32 +0200

    :sparkles: quand le joueur atteint 5100 point il passe a la scene 3_pagewin

--- Assets/PlayerBehavior.cs
+++ Assets/PlayerBehavior.cs
@@ -88,6 +88,10 @@ public class PlayerBehavior : MonoBehaviour
     		direction = Vector2.zero;
     	}
 
+        if (ScoreScript.m_score == 5100){
+            SceneManager.LoadScene("3_PageWin");
+        }
+
     }
 
 }
