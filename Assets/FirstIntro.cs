2a086cc4c79c02624c638e332b12c5a537943751
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-04-23 17:31:47 +0200

    :sparkles: on peut maintenant passer les videos juste au clic

---
+++ Assets/FirstIntro.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+ 
+public class FirstIntro : MonoBehaviour {
+ 
+    public int num;
+ 
+    // Use this for initialization
+    void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(num);
+        }
+    }
+}
