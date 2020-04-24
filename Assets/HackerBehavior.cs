d1ef5ec8fca6d6a643df904c24b7910680924e2e
thunderfreeze <you@example.comyohann.tonnerre@gmail,com>
2020-04-24 12:41:12 +0200

    :sparkles: le hacker tire different virus en fonction de son nombre de vie

--- Assets/HackerBehavior.cs
+++ Assets/HackerBehavior.cs
@@ -16,6 +16,8 @@ public class HackerBehavior : MonoBehaviour
     private float m_speed = 500f;
     public float BulletSpeed = 1000f;
     public Rigidbody2D Bullet;
+    public Rigidbody2D Bullet2;
+    public Rigidbody2D Bullet3;
     private float RouterRate = 2f;
     public Sprite HackerBADCorps;
     public Sprite shooting;
@@ -40,9 +42,21 @@ public class HackerBehavior : MonoBehaviour
     void FixedUpdate()
     {
       	m_rb2D.AddForce(movement * maxSpeed);
-      	 if (Time.time > RouterFire) {
+      	if (Time.time > RouterFire && hackerLife >= 6) {
             RouterFire = Time.time + RouterRate;
             Instantiate(Bullet, transform.localPosition, Quaternion.identity); 
+        	this.gameObject.GetComponent<SpriteRenderer>().sprite = shooting;
+        }
+
+        if (Time.time > RouterFire && hackerLife == 5) {
+            RouterFire = Time.time + RouterRate;
+            Instantiate(Bullet2, transform.localPosition, Quaternion.identity); 
+        	this.gameObject.GetComponent<SpriteRenderer>().sprite = shooting;
+        }
+
+        if (Time.time > RouterFire && hackerLife < 5) {
+            RouterFire = Time.time + RouterRate;
+            Instantiate(Bullet3, transform.localPosition, Quaternion.identity); 
         	this.gameObject.GetComponent<SpriteRenderer>().sprite = shooting;
         }
 
